fb47dc6a1fe5bec75d1b1d13882e2bd00aab0570
Mr. Cormorant <uusers1350@gmail.com>
2023-05-03 17:23:09 +0700

    feat(auth): подготовка к авторизации
    созданы первичные вьюхи и контроллеры, прокинуты роуты

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function register()
+    {
+        return view ('auth.register');
+    }
+
+    public function auth()
+    {
+        dd('lullen');
+    }
+
+    public function newUser()
+    {
+        dd('new lullen');
+    }
+}
