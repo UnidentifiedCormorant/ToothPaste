a5a79d37fd1a1056706d06e031bb7d84f41e72ef
Mr. Cormorant <uusers1350@gmail.com>
2023-05-15 12:23:27 +0700

    fix(pasta): добавлены phpDoc для класса WordsWrapper.php
    проект готов

--- app/Classes/WordsWrapper.php
+++ app/Classes/WordsWrapper.php
@@ -5,32 +5,57 @@ namespace App\Classes;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 
+/**
+ * Подсвечивает слова выбранного для пасты языка красным цветом
+ */
 class WordsWrapper
 {
-    protected $text;
-    protected $keyWords;
+    protected string $text;
+    protected array $keyWords;
 
     public function __construct($text)
     {
         $this->text = $text;
     }
 
+    /**
+     * Вытаскивает английские слова в $keyWords
+     *
+     * @return void
+     */
     protected function getEngWords()
     {
         preg_match_all("((?:[a-zA-Z]+[-']?)*[a-zA-Z]+)", $this->text, $this->keyWords);
     }
 
+    /**
+     * Вытаскивает русские слова в $keyWords
+     *
+     * @return void
+     */
     protected function getRuWords()
     {
         preg_match_all('/([а-я]+)/ui', $this->text, $this->keyWords);
     }
 
+    /**
+     * Заменяет ключевое слово на <font color="red">ключевое слово</font>
+     *
+     * @param $keyWord
+     * @return void
+     */
     protected function replaceWord($keyWord)
     {
         $this->text = Str::replace($keyWord, $this->wrapWord($keyWord), $this->text);
     }
 
-    protected function wrapWord($word)
+    /**
+     * Оборачивает ключевое слово в теги для подсвечивания текста красным цветом
+     *
+     * @param $word
+     * @return string
+     */
+    protected function wrapWord($word) : string
     {
         $word = Str::start($word, '<font color="red">');
         $word = Str::finish($word, '</font>');
@@ -38,7 +63,13 @@ class WordsWrapper
         return $word;
     }
 
-    public function wrap(string $language)
+    /**
+     * Ключевая функция, подсвечвает слова из выбранного для пасты языка
+     *
+     * @param string $language
+     * @return string
+     */
+    public function wrap(string $language) : string
     {
         if ($language == 'ru')
         {
